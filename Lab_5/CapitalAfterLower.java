54bf98f886cb8e1384f511ea1fb6911ff915b98f
Михаил <uralskovm@gmail.com>
2025-11-24 00:22:33 +0300

    Refactor regex replacement in CapitalAfterLower

--- Lab_5/CapitalAfterLower.java
+++ Lab_5/CapitalAfterLower.java
@@ -1,18 +1,19 @@
-import java.util.regex.Pattern;
 import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CapitalAfterLower {
-    public static void main(String[] args) {
-        String text = "abCdeFghIjK";
 
+    public static void main(String[] args) {
+        String text = "aBcdefGhijK";
         String regex = "([a-z])([A-Z])";
+        String result = "";
 
         try {
             Pattern pattern = Pattern.compile(regex);
             Matcher matcher = pattern.matcher(text);
 
-            String result = matcher.replaceAll("!$1$2!");
-
+            result = matcher.replaceAll("$1!$2!");
+            
             System.out.println("Исходный текст: " + text);
             System.out.println("Результат: " + result);
 
